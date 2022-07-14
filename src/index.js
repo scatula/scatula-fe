2f0575754bdcf7ac7e2899140edceebdd457724f
akabolov <alexander.kabolow@gmail.com>
2022-07-14 16:26:43 +0300

    init: add parcel bundler

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
+import { data } from "./mock/mock";
+
+console.log("hello");
+
 const div = document.createElement("div");
-// div.id = "main";
+
 div.innerHTML = "<h1> FREE FILE SHARING </h1>";
 div.style.textAlign = "center";
 div.style.fontWeight = "bold";
 div.style.color = "white";
+
+console.log(data);
+
 document.body.appendChild(div);
