958bb395db7bb7d181a810981b08bb186014682e
brokezm <brokesmail@gmail.com>
2022-07-14 15:52:43 +0300

    Added .js, .css

---
+++ src/javascript.js
@@ -0,0 +1,7 @@
+const div = document.createElement("div");
+// div.id = "main";
+div.innerHTML = "<h1> FREE FILE SHARING </h1>";
+div.style.textAlign = "center";
+div.style.fontWeight = "bold";
+div.style.color = "white";
+document.body.appendChild(div);
